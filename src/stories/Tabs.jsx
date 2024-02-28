0790948abb90d74abaa46331bd27b47a2014e4d3
Alba Blanca Lopez Melian <alba.lopezmelian@plexus.tech>
2024-02-28 11:52:52 +0100

    Start deploy

---
+++ src/stories/Tabs.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import './tabs.css';
+import { LuUserCog } from "react-icons/lu";
+import { MdOutlineTheaterComedy } from "react-icons/md";
+import { LuGlasses } from "react-icons/lu";
+import { PiUserListLight } from "react-icons/pi";
+
+export const Tabs = ({currentTabs}) => {
+
+  return (
+    <div className='tabs-container'>
+        <div>
+            <div className='tabs'>
+                <button
+                    className='button-tabs-active' 
+                    id='1'
+                    currentTabs={currentTabs}>
+                    <div>
+                        <LuUserCog fontSize='20px' />
+                    </div>                
+                    Gestionar Beneficiarios
+                </button>
+                <button
+                    className='button-tabs' 
+                    id='2'
+                    currentTabs={currentTabs}>
+                    <div>
+                        <MdOutlineTheaterComedy  fontSize='20px'  />
+                    </div>
+                    Gestionar Excepciones
+                </button>
+                <button
+                    className='button-tabs' 
+                    id='3'
+                    currentTabs={currentTabs}>
+                    <div>
+                        <LuGlasses  fontSize='20px' />
+                    </div>
+                    ¿Quién tiene qué?
+                </button>
+                <button
+                    className='button-tabs' 
+                    id='4'
+                    currentTabs={currentTabs}>
+                    <div>
+                        <PiUserListLight  fontSize='20px' />
+                    </div>
+                    Dar Acceso
+                </button>
+            </div>
+            <div>
+                <div className='content-tabs'>
+                    <input className='tabs-input' type="text" value='Nº Emp.'/>
+                    <input className='tabs-input' type="text" value='Nombre'/>
+                </div>
+                <div className='content-tabs'>
+                    <input className='tabs-input' type="text" value='Primer Apellido'/>
+                    <input className='tabs-input' type="text" value='Segundo Apellido'/>
+                </div>
+                <div className='tabs-content-select'>
+                    <div >
+                        <label className="tabs-label-search">Estado</label>
+                        <select className='tabs-select-search'>
+                            <option> Pendientes</option>
+                            <option>option 1</option>
+                            <option>option 2</option>
+                        </select>
+                    </div>
+                    <div className='tabs-content-search'>
+                        <label className="tabs-label-search">Usuarios por página</label>
+                        <select className='tabs-select-search left'>
+                            <option> 10</option>
+                            <option>option 1</option>
+                            <option>option 2</option>
+                        </select>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="tabs-table">
+                <table>
+                    <tr className='tabs-tr'>
+                        <th className='th-tabs'>Nº Emp</th>
+                        <th className='th-tabs'>Nombre</th>
+                        <th className='th-tabs'>Departamento</th>
+                        <th className='th-tabs'>Fecha de Solicitud</th>
+                        <th className='th-tabs'>Estado</th>
+                        <th className='th-tabs'></th>
+                    </tr>
+                    <tr>
+                        <td>102944</td>
+                        <td>Monederos Yebenes, Miguel</td>
+                        <td>Flota B737</td>
+                        <td>21/02/2024 01:00</td>
+                        <td>Pendiente</td>
+                        <td><button>Gestionar</button>  </td>
+                    </tr>
+                    <tr>
+                    <td>117176</td>
+                        <td>Tisci, Simona</td>
+                        <td>Reclamaciones Oficiales</td>
+                        <td>21/02/2024 01:00</td>
+                        <td>Pendiente</td>
+                        <td><button>Gestionar</button>  </td>
+                    </tr>
+                </table>
+            </div>
+        </div>   
+    </div>
+  )
+}
